6e35a231545f23d28daa64c265bd5754e7eaf509
Somnath Chakraborty <somchakraborty98@gmail.com>
2025-12-20 19:02:30 +0530

    2nd commit

--- os_module/os_combined_with_other_libs.py
+++ os_module/os_combined_with_other_libs.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 ## 1. os + shutil: Automate file backups and cleanup
 # Create dummy file and backup directory
-## file name
 file_to_backup = "data.txt"
 backup_dir = "backup"
 
